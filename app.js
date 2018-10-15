84694db5da0122128071418c23b7685f49f2c603
Rafa <rafaelruizbernal@gmail.com>
2018-10-15 19:04:16 +0100

    board done

--- app.js
+++ app.js
@@ -1,55 +1,99 @@
-// let guy = document.getElementById('guy');
-// let container = document.getElementsByClassName('container');
 
 
+const board = [true, false, true, true, true, true, true, true, true, true, true,
+  true, false, true, false, false, false, false, true, false, true, true, true,false, true, false, false, false, false, true, false, false, true, true, true, true, true, true, true,true, true, true, true, true, true, true, false, false, true, true, true, true, true, true, true, true, true, false, true, true, true, true, true, false, true, true, false, false, false, false, true, true, true, true, false, true, true, true, true, true, true, true, true,true, true, false, false, true, true, true, false, false, false, false, true, true, false, true, true, true, true, true, true, true, true, true, true, false, true, true];
 
-window.addEventListener('keydown', function(e) {
-  console.log(e.which);
-  if (e.which === 38) {
+
+window.addEventListener('keydown', function(event) {
+
+  if (event.which === 38) {
     moveUp();
-  } else if (e.which === 40) {
+
+  } else if (event.which === 40) {
     moveDown();
-  } else if (e.which === 37) {
+  } else if (event.which === 37) {
     moveLeft();
-  } else if (e.which === 39) {
+  } else if (event.which === 39) {
     moveRight();
   }
 });
 
 const $allSquares = $('.square');
-let guy2Pos = 35;
-let guy2 = document.querySelectorAll('.square')[guy2Pos];
+
+const guy2Pos = 109;
+const guy3Pos = 20;
+const guy4Pos = 58;
+const guy5Pos = 89;
+const guy6Pos = 52;
+// const guy2Pos = Math.floor(Math.random()*110);
+// const guy3Pos = Math.floor(Math.random()*110);
+// const guy4Pos = Math.floor(Math.random()*110);
+// const guy5Pos = Math.floor(Math.random()*110);
+const guy2 = document.querySelectorAll('.square')[guy2Pos];
 guy2.classList.add('guy2');
+const guy3 = document.querySelectorAll('.square')[guy3Pos];
+guy3.classList.add('guy3');
+const guy4 = document.querySelectorAll('.square')[guy4Pos];
+guy4.classList.add('guy4');
+const guy5 = document.querySelectorAll('.square')[guy5Pos];
+guy5.classList.add('guy5');
+const guy6 = document.querySelectorAll('.square')[guy6Pos];
+guy6.classList.add('guy6');
+
 
 let guyPos = 0;
 let guy1 = document.querySelectorAll('.square')[guyPos];
 guy1.classList.add('guy');
-// leftButton.addEventListener('click', moveLeft);
-// rightButton.addEventListener('click', moveRight);
+
 
 function moveRight(){
-  guy1.classList.remove('guy');
-  guyPos++;
-  guy1 = document.querySelectorAll('.square')[guyPos];
-  guy1.classList.add('guy');
+  if (board[guyPos + 1]){
+    guy1.classList.remove('guy');
+    guyPos++;
+    guy1 = document.querySelectorAll('.square')[guyPos];
+    guysKilled();
+  } else {
+    return false;
+  }
+
 }
 function moveLeft(){
-  guy1.classList.remove('guy');
-  guyPos--;
-  guy1 = document.querySelectorAll('.square')[guyPos];
-  guy1.classList.add('guy');
+
+  if (board[guyPos - 1]){
+    guy1.classList.remove('guy');
+    guyPos--;
+    guy1 = document.querySelectorAll('.square')[guyPos];
+    guysKilled();
+  } else {
+    return false;
+  }
 }
 function moveUp(){
-  guy1.classList.remove('guy');
-  guyPos-=6;
-  guy1 = document.querySelectorAll('.square')[guyPos];
-  guy1.classList.add('guy');
+  if (board[guyPos - 11]){
+    guy1.classList.remove('guy');
+    guyPos-=11;
+    guy1 = document.querySelectorAll('.square')[guyPos];
+    guysKilled();
+  } else {
+    return false;
+  }
 }
 function moveDown(){
-  guy1.classList.remove('guy');
-  guyPos+=6;
-  guy1 = document.querySelectorAll('.square')[guyPos];
-  guy1.classList.add('guy');
+  if (board[guyPos + 11]){
+    guy1.classList.remove('guy');
+    guyPos+=11;
+    guy1 = document.querySelectorAll('.square')[guyPos];
+    guysKilled();
+  } else {
+    return false;
+  }
+
 }
-//
-// document.addEventListener('click', moveRight);
+function guysKilled(){
+  if (guy1.classList.contains('guy2') || guy1.classList.contains('guy3') || guy1.classList.contains('guy4') || guy1.classList.contains('guy5') || guy1.classList.contains('guy6')) {
+    alert('u got him!!');
+    guy1.className = '';
+
+  }
+  guy1.classList.add('guy', 'square');
+}
